b9ff33a8bcc50f3ab4a3750344dc7e2000b06129
Michaela Lehr <michaela.lehr@geildanke.com>
2021-07-08 20:22:20 +0200

    Initial Commit

---
+++ Trimmy WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trimmy WatchKit Extension
+//
+//  Created by Michaela Lehr on 08.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trimmy WatchKit Extension/TrimmyApp.swift
@@ -0,0 +1,19 @@
+//
+//  TrimmyApp.swift
+//  Trimmy WatchKit Extension
+//
+//  Created by Michaela Lehr on 08.07.21.
+//
+
+import SwiftUI
+
+@main
+struct TrimmyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
